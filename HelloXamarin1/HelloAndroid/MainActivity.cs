0ceb6e1e04ee694ab0b6f4235aa4f06d07567785
LBugnion <LBugnion@valorem.com>
2017-02-25 20:36:23 +0100

    Added HelloXamarin 1

---
+++ HelloXamarin1/HelloAndroid/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HelloAndroid
+{
+    [Activity(Label = "HelloAndroid1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private int _counter;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            var button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += (s, e) =>
+            {
+                var text = FindViewById<TextView>(Resource.Id.MyText);
+                text.Text = $"Counter clicked {++_counter} times";
+            };
+        }
+    }
+}
+
