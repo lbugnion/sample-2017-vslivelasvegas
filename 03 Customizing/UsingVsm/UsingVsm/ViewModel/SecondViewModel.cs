490fd02b910f82c990b98cf42041a8972699417d
LBugnion <LBugnion@valorem.com>
2017-03-01 15:44:50 -0600

    Added UsingVsm

---
+++ 03 Customizing/UsingVsm/UsingVsm/ViewModel/SecondViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace UsingVsm.ViewModel
+{
+    public class SecondViewModel
+    {
+        public List<string> Quotes
+        {
+            get
+            {
+                var list = new List<string>
+                {
+                    "Love all, trust a few, do wrong to none.",
+                    "You speak an infinite deal of nothing.",
+                    "With mirth and laughter let old wrinkles come.",
+                    "Conscience doth make cowards of us all.",
+                    "Et tu, Brute?",
+                    "Wisely and slow; they stumble that run fast.",
+                    "Some are born great, others achieve greatness"
+                };
+
+                return list;
+            }
+        }
+    }
+}
