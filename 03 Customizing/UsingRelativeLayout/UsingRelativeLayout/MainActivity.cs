acd6224ad3b69e04107951894bf195d89d492365
LBugnion <LBugnion@valorem.com>
2017-03-12 10:08:44 -0700

    Added UsingStackLayout and UsingRelativeLayout

---
+++ 03 Customizing/UsingRelativeLayout/UsingRelativeLayout/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace UsingRelativeLayout
+{
+    [Activity(Label = "UsingRelativeLayout", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            // TODO Code to switch on orientation change
+        }
+    }
+}
+
